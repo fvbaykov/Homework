6dfd1411d6e987128dcb27fb91f5abdb73e1900b
fedorbajkov <baykov3105@mail.ru>
2016-12-05 19:28:48 +0300

    added hw8

---
+++ HW8/1.py
@@ -0,0 +1,16 @@
+way=input('Введите путь к файлу без дополнительных символов: ', )
+f=open(way, 'r', encoding = 'utf-8')
+text=f.read()
+f.close()
+min=len(text)
+max=0
+arr = text.split('\n')
+for el in arr:
+    if len(el) > max:
+        max = len(el)
+    if len(el) < min:
+        min = len(el)
+k = max/min
+print('Самая короткая строка короче самой длинной в ', k, ' раз(а)')
+
+
